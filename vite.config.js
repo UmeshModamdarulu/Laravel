5cb23d676b5dcfd457835591f1af6f12ebc1f8ba
UmeshModamdarulu <117798247+UmeshModamdarulu@users.noreply.github.com>
2025-08-09 05:20:51 +0000

    env example changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'super-duper-palm-tree-56gr4wwgxqvfpg67-5173.app.github.dev', // your Codespaces domain
+            protocol: 'wss',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
